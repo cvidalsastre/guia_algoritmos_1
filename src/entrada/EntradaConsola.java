b646d16569dcf664c60af4a54cd9da7d7d9192ea
peronvidal <cristian.vidalsastre@gmail.com>
2024-10-12 15:40:45 -0300

    prueba entrada salida

---
+++ src/entrada/EntradaConsola.java
@@ -0,0 +1,17 @@
+package entrada;
+import java.util.Scanner;
+import salida.SalidaConsola;
+
+public class EntradaConsola {
+    public EntradaConsola (Integer cantidadEntradas) {
+       String[] entradas = new String[cantidadEntradas];
+       Scanner sc = new Scanner(System.in);
+         for (int i = 0; i < cantidadEntradas; i++) {
+              System.out.println("Ingrese la entrada " + (i+1));
+              entradas[i] = sc.nextLine();
+              SalidaConsola.mostrarEnPantalla(entradas[i]);
+         }
+        sc.close();
+        
+    }
+}
